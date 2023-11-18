3a3d8502d2a0465547ea8c4653def64336e052b3
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2023-11-19 01:01:01 +0300

    feat: add adaptive

---
+++ utils/textValidation.js
@@ -0,0 +1,7 @@
+// const textElement = document.querySelectorAll(".card__title");
+
+// textElement.forEach((text) => {
+//   if (text.textContent.length > 12) {
+//     text.classList.add("card__title_ellipsis");
+//   }
+// });
